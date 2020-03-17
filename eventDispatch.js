1b707d08b7a4aeadf89b0695f6d2f6cfe70d8884
xiepengfei.alpaca <xiepengfei.alpaca@bytedance.com>
2020-03-17 17:19:32 +0800

    uodate: 一百年过去了，于是礼貌的更新下

--- eventDispatch.js
+++ eventDispatch.js
@@ -1,27 +1,69 @@
-/**
-* 定义事件分发对象
-**/
+function Event() {
+  this.eventPool = {};
+  this.TYPE = {
+    normal: 1,
+    once: 2
+  };
+}
 
-export const eventDispatch = {
-	//事件队列
-	eventList:{},
-
-	//on事件
-	on:function(eventName, callback) {
-		this.eventList[eventName] = callback;
-	},
-
-	//emit事件
-	emit:function(eventName) {
-		if(arguments.length < 1) {
-			//是否存在事件
-			if(this.eventList[eventName]) this.eventList[eventName]();
-		}else{
-			var params = Array.prototype.slice.call(arguments, 1);
-			if(this.eventList[eventName]) this.eventList[eventName].apply(this, params);
-		}
-	}
+Event.prototype = {
+  addCallback(eventName, type, callback) {
+    if (!this.eventPool[eventName]) {
+      this.eventPool[eventName] = {
+        callbacks: [],
+        hasOnce: false
+      };
+    }
+    if (typeof callback === 'function') {
+      this.eventPool[eventName].callbacks.push({
+        type: type,
+        callback: callback
+      });
+      this.eventPool[eventName].hasOnce = type === this.TYPE.once;
+    } else {
+      console.warn('the callback arg shoule be function!');
+    }
+  },
+  // 常规监听
+  on: function(eventName, callback) {
+    this.addCallback(eventName, this.TYPE.normal, callback);
+  },
+  // 监听的事件只触发一次
+  once: function(eventName, callback) {
+    this.addCallback(eventName, this.TYPE.once, callback);
+  },
+  // 触发事件
+  emit: function(eventName) {
+    if (!this.eventPool[eventName]) {
+      return false;
+    }
+    var args = Array.prototype.slice.call(arguments, 1);
+    this.eventPool[eventName].callbacks.forEach(function(item) {
+      item.callback.apply(this, args);
+    });
+    // 清除once
+    if (this.eventPool[eventName].hasOnce) {
+      this.clearOnce(eventName);
+    }
+  },
+  // 删除指定事件的once callback
+  clearOnce: function(eventName) {
+    var newList = [];
+    var list = this.eventPool[eventName].callbacks;
+    for (var i = 0; i < list.length; i++) {
+      if (list[i].type !== this.TYPE.once) {
+        newList.push(list[i]);
+      }
+    }
+    this.eventPool[eventName].callbacks = [].concat(newList);
+    this.eventPool[eventName].hasOnce = false;
+  },
+  // 清楚事件的所有的callback
+  clear: function(eventName) {
+    if (!!this.eventPool[eventName]) {
+      this.eventPool[eventName] = null;
+    }
+  }
 };
 
-
-
+export default Event;
